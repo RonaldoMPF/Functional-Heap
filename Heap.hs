89f97d63a7d8f94b3e8e8a416d4cf02602ba9ee2
RonaldoMPF <ronaldo.medeiros.filho@ccc.ufcg.edu.br>
2018-07-18 02:16:02 -0300

    add merge an makeHP

--- Heap.hs
+++ Heap.hs
@@ -13,6 +13,20 @@ module Heap (Heap, emptyHeap, heapEmpty, findHeap, insHeap, delHeap) where
   findHeap EmptyHP = error "findHeap: empty Heap"
   findHeap (HP x a b) = x
 
+  rank :: (Ord a) => Heap a -> Int
+  rank EmptyHP = 0
+  rank (HP _ r _ _ ) = r
+
+  makeHP :: (Ord a) => a -> Heap a -> Heap a -> Heap a
+  makeHP x a b | rank a >= rank b = HP x (rank b + 1) a b
+               | otherwise = HP x (rank a + 1) b a
+
+  merge :: (Ord a) => Heap a -> Heap a -> Heap a
+  merge h EmptyHP = h
+  merge EmptyHP h = h
+  merge h1@(HP x _ a1 b1) h2@(HP y _ a2 b2) | x <= y = makeHP x a1 (merge b1 h2)
+                                            | otherwise = makeHP y a2 (merge h1 b2)
+
   insHeap :: (Ord a) => a -> Heap a -> Heap a
 
   delHeap :: (Ord a) => Heap a -> Heap a
