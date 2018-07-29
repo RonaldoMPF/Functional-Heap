80033c4c51743b7ba1308657d912356dab29cd7e
RonaldoMPF <ronaldo.medeiros.filho@ccc.ufcg.edu.br>
2018-07-28 21:20:52 -0300

    add min-heap max-heap support

--- Heap.hs
+++ Heap.hs
@@ -1,56 +1,63 @@
 module Heap (Heap, emptyHeap, heapEmpty, findHeap, insHeap, delHeap, buildHeap, heapSort) where
 
-  data Heap a = EmptyHP | HP a Int (Heap a) (Heap a) deriving Show
+  data Heap a = EmptyHP Ordering | HP a Int Ordering (Heap a) (Heap a) deriving Show
 
-  emptyHeap :: Heap a
-  emptyHeap :: Heap a
-  emptyHeap = EmptyHP
+  emptyHeap :: Ordering -> Heap a
+  emptyHeap ordering = (EmptyHP ordering)
 
   heapEmpty :: Heap a -> Bool
-  heapEmpty EmptyHP = True
+  heapEmpty (EmptyHP _ ) = True
   heapEmpty _ = False
 
   findHeap :: (Ord a) => Heap a -> a
-  findHeap EmptyHP = error "findHeap: Empty Heap!"
-  findHeap (HP element _ left right) = element
+  findHeap (EmptyHP _ ) = error "findHeap: Empty Heap!"
+  findHeap (HP element _ _ left right) = element
 
   rank :: (Ord a) => Heap a -> Int
-  rank EmptyHP = 0
-  rank (HP _ rank _ _ ) = rank
+  rank (EmptyHP _ ) = 0
+  rank (HP _ rank _ _ _ ) = rank
+
+  policy :: (Ord a) => Heap a -> Ordering
+  policy (EmptyHP policy) =  policy
+  policy (HP _ _ policy _ _) = policy
+
 
   -- Swap children nodes if rank(right) > rank(left)
   makeHP :: (Ord a) => a -> Heap a -> Heap a -> Heap a
-  makeHP x a b | rank a >= rank b = HP x (rank b + 1) a b
-               | otherwise = HP x (rank a + 1) b a
+  makeHP x a b | (policy a /= policy b) = error "make HP: diferent heap policies"
+               | rank a >= rank b = HP x (rank b + 1) (policy a) a b
+               | otherwise = HP x (rank a + 1) (policy b) b a
 
   -- O(log n) merge
   merge :: (Ord a) => Heap a -> Heap a -> Heap a
-  merge heap EmptyHP = heap
-  merge EmptyHP heap = heap
-  merge heap1@(HP element1 _ left1 right1) heap2@(HP element2 _ left2 right2)
-    | element1 <= element2 = makeHP element1 left1 (merge right1 heap2)
+  merge heap (EmptyHP _ ) = heap
+  merge (EmptyHP _ ) heap = heap
+  merge heap1@(HP element1 _ policy1 left1 right1) heap2@(HP element2 _ policy2 left2 right2)
+    | (policy heap1 /= policy heap2) = error "merge: diferent heap policies"
+    | element1 `compare` element2 == policy1 = makeHP element1 left1 (merge right1 heap2)
     | otherwise = makeHP element2 left2 (merge heap1 right2)
 
   -- Merge Heap with root node containing element - O(log n)
   insHeap :: (Ord a) => a -> Heap a -> Heap a
-  insHeap element heap = merge (HP element 1 EmptyHP EmptyHP) heap
+  insHeap element heap = merge (HP element 1 policyhp (emptyHeap policyhp) (emptyHeap policyhp)) heap where
+    policyhp = (policy heap)
 
   -- Merge root childrens - O(log n)
   delHeap :: (Ord a) => Heap a -> Heap a
-  delHeap EmptyHP = error "delHeap: Empty Heap!"
-  delHeap (HP element _ left right) = merge left right
+  delHeap (EmptyHP _ ) = error "delHeap: Empty Heap!"
+  delHeap (HP element _ _ left right) = merge left right
 
   -- O(n log n) build heap
-  buildHeap :: (Ord a) => [a] -> Heap a
-  buildHeap list = buildHeapAux list emptyHeap
+  buildHeap :: (Ord a) => [a] -> Ordering -> Heap a
+  buildHeap (x:xs) ordering = buildHeapAux xs (HP x 1 ordering (emptyHeap ordering) (emptyHeap ordering) )
 
   buildHeapAux :: (Ord a) => [a] -> Heap a -> Heap a
   buildHeapAux [] heap = heap
   buildHeapAux (x:xs) heap = buildHeapAux xs (insHeap x heap)
 
   -- O(n log n) sort list
-  heapSort :: (Ord a) => [a] -> [a]
-  heapSort list = heapSortAux (buildHeap list)
+  heapSort :: (Ord a) => [a] -> Ordering -> [a]
+  heapSort list ordering = heapSortAux (buildHeap list ordering)
 
   heapSortAux :: (Ord a) => Heap a -> [a]
   heapSortAux heap | heapEmpty heap == True = []
